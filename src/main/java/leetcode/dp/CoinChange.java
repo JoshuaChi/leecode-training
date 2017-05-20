c83f673ba0601fe4ccf7eaa86933b037d58da3ad
joshokn <joshokn@gmail.com>
2017-05-20 21:41:44 +0800

    coin change

--- src/main/java/leetcode/dp/CoinChange.java
+++ src/main/java/leetcode/dp/CoinChange.java
@@ -57,4 +57,15 @@ public class CoinChange {
         return false;
 
     }
+    public static long makeChange2(int[] coins, int money) {
+        long[] DP = new long[money + 1]; // O(N) space.
+        DP[0] = (long) 1; 	// n == 0 case.
+        for(int coin : coins) {
+            for(int j = coin; j < DP.length; j++) {
+                // The only tricky step.
+                DP[j] += DP[j - coin];
+            }
+        }
+        return DP[money];
+    }
 }
