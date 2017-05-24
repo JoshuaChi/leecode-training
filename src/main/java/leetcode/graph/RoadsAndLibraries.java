37b2cdec9b939a9fc35a71613458aae374694088
Joshua Chi <joshua.chi@akqa.com>
2017-05-24 18:53:24 +0800

    commit a correct one for graph

--- src/main/java/leetcode/graph/RoadsAndLibraries.java
+++ src/main/java/leetcode/graph/RoadsAndLibraries.java
@@ -19,40 +19,61 @@ public class RoadsAndLibraries {
             int m = in.nextInt(); // roads
             int x = in.nextInt(); // lib cost
             int y = in.nextInt(); // road cost
+            HashMap<Long, HashSet<Long>> cities = new HashMap<>();
 
-            HashSet<Integer>[] cities = new HashSet[n];
-
-            for (int i=0; i<cities.length; i++) {
-                cities[i] = new HashSet<>();
-            }
             for (int a1 = 0; a1 < m; a1++) {
-                int city_1 = in.nextInt(); //city 1 is connected with city 2
-                int city_2 = in.nextInt();
+                long city_1 = in.nextInt(); //city 1 is connected with city 2
+                long city_2 = in.nextInt();
 
-                int i = city_1 - 1;
-                int e = city_2 - 1;
+                long i = city_1 - 1;
+                long e = city_2 - 1;
 
-                cities[i].add(e);
-                cities[e].add(i);
+                init(cities, e, i);
+                init(cities, i, e);
             }
-            HashSet<Integer> visited = new HashSet<>();
-            long cost = cal(cities, x, y, visited);
-            for (int i = 0; i < cities.length; i++) {
-                if (cities[i].isEmpty()) {
-                    cost += x;
+
+            long cost = x * n;
+            if(x > y) {
+                HashSet<Long> visited = new HashSet<>();
+                cost = cal(cities, x, y, visited);
+
+                for (long i = 0; i < n; i++) {
+                    if (cities.containsKey(i) == false) {
+                        cost += x;
+                    }
                 }
+
             }
+
             System.out.println(cost);
         }
         in.close();
     }
 
-    private static long cal(HashSet<Integer>[] cities, int libCost, int roadCost, HashSet<Integer> visited) {
-        int cost = 0;
-        for (HashSet<Integer> city : cities) {
+    private static void init(HashMap<Long, HashSet<Long>> cities, Long i, Long e) {
+        if (cities.containsKey(e)) {
+            HashSet<Long> set = cities.get(e);
+            set.add(i);
+            cities.put(e, set);
+        }
+        else {
+            HashSet<Long> set = new HashSet<>();
+            set.add(i);
+            cities.put(e, set);
+        }
+    }
+
+    private static long cal(HashMap<Long, HashSet<Long>> cities, int libCost, int roadCost, HashSet<Long> visited) {
+        long cost = 0;
+        Iterator i = cities.entrySet().iterator();
+        while(i.hasNext()) {
+            Map.Entry<Long, HashSet<Long>> entry = (Map.Entry<Long, HashSet<Long>>)i.next();
+            Long city = entry.getKey();
+            HashSet<Long> set = entry.getValue();
+
             if (visited.contains(city) == false) {
-                final int[] count = new int[1];
-                city.forEach(x -> DFS(cities, x, visited, count));
+                final long[] count = new long[1];
+                set.forEach(x -> DFS(cities, x, visited, count));
                 if (count[0] > 0) {
                     if (roadCost >= libCost) {
                         cost += count[0] * libCost;
@@ -62,7 +83,6 @@ public class RoadsAndLibraries {
                 }
             }
         }
-
         return cost;
     }
 
@@ -70,15 +90,15 @@ public class RoadsAndLibraries {
         return String.format("%s-%s", x, y);
     }
 
-    private static void DFS(HashSet<Integer>[] cities, Integer city, HashSet<Integer> visited, final int[] count) {
+    private static void DFS(HashMap<Long, HashSet<Long>> cities, Long city, HashSet<Long> visited, final long[] count) {
 
         if (visited.contains(city)) {
             return;
         }
         visited.add(city);
-        if (cities[city].isEmpty() == false) {
+        if (cities.containsKey(city)) {
             count[0] ++;
-            HashSet<Integer> set = cities[city];
+            HashSet<Long> set = cities.get(city);
             set.forEach(i -> {
                         if (visited.contains(i) == false) {
                             DFS(cities, i, visited, count);
