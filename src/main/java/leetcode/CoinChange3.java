73f6622236fbf74c4ae1b1d809dac48933af5d1c
joshokn <joshokn@gmail.com>
2017-06-11 14:12:49 +0800

    add coin change 3

--- src/main/java/leetcode/CoinChange3.java
+++ src/main/java/leetcode/CoinChange3.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.Arrays;
+
 /**
  * Created by Joshua on 6/8/17.
  * LC:322
@@ -18,11 +20,22 @@ package leetcode;
  */
 public class CoinChange3 {
     public int coinChange(int[] coins, int amount) {
-        return dp(coins, amount);
+        int[] cache = new int[amount+1];
+        Arrays.fill(cache, -1);
+        return dp(coins, amount, cache);
     }
 
-    public int dp(int[] coins, int amount) {
+    public int dp(int[] coins, int amount, int[] cache) {
+        if (-1 != cache[amount]) {
+            return cache[amount];
+        }
+
+        if (amount < 0) {
+            return -1;
+        }
+
         if (amount == 0) {
+            cache[0] = 0;
             return 0;
         }
 
@@ -31,21 +44,24 @@ public class CoinChange3 {
             if (coins[i] > amount) {
                 continue;
             }
-            int count = dp(coins, amount-coins[i]);
-            if (count >=0 && (count + 1) < min) {
+            int count = dp(coins, amount-coins[i], cache);
+            if (count != Integer.MAX_VALUE && count >=0 && (count + 1) < min) {
                 min = count + 1;
             }
         }
-        return min == Integer.MAX_VALUE? -1: min;
+        cache[amount] = min;
+        if (min != Integer.MAX_VALUE) {
+            return min;
+        }
+        return -1;
     }
-
 
 
     public static void main(String[] args) {
 
         CoinChange3 c = new CoinChange3();
-        int[] coins = new int[]{1,2,5};
-        int count = c.coinChange(coins, 4);
+        int[] coins = new int[]{442,295,365,485};
+        int count = c.coinChange(coins, 8091);
         System.out.print(count);
     }
 }
